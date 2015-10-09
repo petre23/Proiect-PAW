c22faee1b84f976a637cc28af34e9f797b486bd4
petre23 <mazilu_petre@yahoo.ro>
2015-10-09 19:47:29 +0300

    Project Base
    Project Base

---
+++ GamingSite/GamingSite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using GamingSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamingSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Game";
+            Game gameInfo = new Game();
+            gameInfo.Name = "Call Of Duty Black Ops 3";
+            gameInfo.Genre = "FPS";
+            gameInfo.Rating = (float)8.9;
+            gameInfo.ReleaseYear = 2015;
+            gameInfo.ImageUrl = "http://blogs-images.forbes.com/insertcoin/files/2015/08/black-ops-3-beta1.jpg";
+
+            return View(gameInfo);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GamingSite/GamingSite/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamingSite.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public float Rating { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
